585b4b3347e702a314b4a876f0e3d7da31d8a448
Artur Kowalczyk <artur.kowalczyk96@gmail.com>
2021-07-13 13:51:52 +0200

    6.2 What is TDD - testing add and subtract.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,44 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
 
     public static void main(String[] args) {
 
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        Calculator calculator = new Calculator(6, 4);
+
+        String result = simpleUser.getUsername();
+        int sum = calculator.addAToB();
+        int subtraction = calculator.subtractBFromA();
+
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //Test
+        System.out.println("Test 1. Adding");
+
+        if (sum == 10) {
+            System.out.println("Test 1. ok");
+        } else {
+            System.out.println("Error - test 1.");
+        }
+
+        //Test
+        System.out.println("Test 2. Adding");
+
+        if (subtraction == 2) {
+            System.out.println("Test 2. ok");
+        } else {
+            System.out.println("Error - test 2.");
+        }
+
     }
 }
