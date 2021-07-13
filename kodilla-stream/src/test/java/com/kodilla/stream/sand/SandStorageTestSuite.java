8506eda37ee7a088804f1e6e43a9d3275e2a0be0
Artur Kowalczyk <artur.kowalczyk96@gmail.com>
2021-07-14 00:29:05 +0200

    7.4 Stream functions - reduce()

--- kodilla-stream/src/test/java/com/kodilla/stream/sand/SandStorageTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/sand/SandStorageTestSuite.java
@@ -11,17 +11,17 @@ public class SandStorageTestSuite {
     private static int testCounter = 0;
 
     @BeforeAll
-    public static void beforeAllTests(){
+    public static void beforeAllTests() {
         System.out.println("The beginning of tests");
     }
 
     @AfterAll
-    public static void afterAllTests(){
+    public static void afterAllTests() {
         System.out.println("The end of all tests");
     }
 
     @BeforeEach
-    public void beforeEveryTest(){
+    public void beforeEveryTest() {
         testCounter++;
         System.out.println("Execute test #" + testCounter);
     }
@@ -40,6 +40,25 @@ public class SandStorageTestSuite {
         for (SandStorage continent : continents) {
             totalSand = totalSand.add(continent.getSandBeansQuantity());
         }
+
+        //Then
+        BigDecimal expectedSand = new BigDecimal("211111110903703703670");
+        Assertions.assertEquals(expectedSand, totalSand);
+    }
+
+    @DisplayName("Test: getSandBeansQuantityWithReduce()")
+    @Test
+    void testGetSandBeansQuantityWithReduce() {
+        //Given
+        List<SandStorage> continents = new ArrayList<>();
+        continents.add(new Europe());
+        continents.add(new Asia());
+        continents.add(new Africa());
+
+        //When
+        BigDecimal totalSand = continents.stream()
+                .map(SandStorage::getSandBeansQuantity)
+                .reduce(BigDecimal.ZERO, (sum, current) -> sum = sum.add(current));
 
         //Then
         BigDecimal expectedSand = new BigDecimal("211111110903703703670");
